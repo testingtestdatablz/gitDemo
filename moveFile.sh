71932bf758f8201ad72d2aaafeffefc44007d2be
sheetalc818 <sheetalc818@gmail.com>
2023-06-06 22:17:26 +0530

    Modified : file

--- moveFile.sh
+++ moveFile.sh
@@ -3,7 +3,10 @@ for file in `ls *.txt`
 do
 	folderName=`echo $file | awk -F . '{print $1}'`;
 	echo $folderName;
-   	rm -r $folderName;
+	if [ -d $folderName ]
+	then
+   		rm -r $folderName;
+	fi
 	mkdir $folderName;
    cp $file $folderName;
    echo Copied $file into $folderName
