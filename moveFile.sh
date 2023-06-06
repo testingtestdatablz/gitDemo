daaa3223ce6cc60e2db85d654724ee2cfcf446be
testingtestdatablz <135731353+testingtestdatablz@users.noreply.github.com>
2023-06-06 22:15:37 +0530

    Update moveFile.sh

--- moveFile.sh
+++ moveFile.sh
@@ -1,10 +1,13 @@
 #!/bin/bash -x
 for file in `ls *.txt`
 do
-	folderName=`echo $file | awk -F . '{print $1}'`;
-	echo $folderName;
-   	rm -r $folderName;
-	mkdir $folderName;
+   folderName=`echo $file | awk -F . '{print $1}'`;
+   echo $folderName;
+	if [ -d $folderName ]
+	then
+   		rm -r $folderName;
+		mkdir $folderName;
+	fi
    cp $file $folderName;
    echo Copied $file into $folderName
 done
