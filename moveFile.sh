7e89ea1e2a426c58dd3ef85a2e7bae63b96ffe1a
sheetalc818 <sheetalc818@gmail.com>
2023-06-13 13:34:49 +0530

    ADDED : Print file variable

--- moveFile.sh
+++ moveFile.sh
@@ -3,6 +3,7 @@ for file in `ls *.txt`
 do
    folderName=`echo $file | awk -F . '{print $1}'`;
    echo $folderName;
+	echo $file
         if [ -d $folderName ]
         then
                 rm -r $folderName;
